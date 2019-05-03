eafaf94d2543f1cbd4f337f17f29a59dffbf6eac
Ranika Williams <rnkprice07@gmail.com>
2019-05-03 17:14:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\A{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
